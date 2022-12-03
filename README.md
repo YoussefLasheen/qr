173d95b5653c0ec3d293269f02117454244395cc
Youssef Lasheen <youssef@lasheen.dev>
2022-12-03 20:05:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# qr reader
+
+A beautiful material you qr scanner/generator app with absolutely no ads for Android, Linux, and MacOS.
---
+++ android/app/src/main/kotlin/dev/lasheen/qr/qr/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.lasheen.qr.qr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
