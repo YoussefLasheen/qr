38ff46a1a896055a97363b710c9f6d97bf636bd5
Youssef Lasheen <youssef@lasheen.dev>
2022-12-04 13:41:19 +0200

    Add a file picker with file_selector

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:qr/screens/camera_screen.dart';
 
+import 'package:file_selector/file_selector.dart';
+import 'package:qr/screens/scanner_screen.dart';
+
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () => Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const CameraScreen()),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const CameraScreen()),
+          ),
+          child: const Text('Open Camera'),
         ),
-        child: const Text('Open Camera'),
-      ),
+        SizedBox(width: 20),
+        ElevatedButton(
+          onPressed: () async {
+            const XTypeGroup typeGroup = XTypeGroup(
+              label: 'images',
+              extensions: <String>['jpg', 'png'],
+            );
+            final XFile? file =
+                await openFile(acceptedTypeGroups: <XTypeGroup>[typeGroup]);
+            if (file == null) {
+              // Operation was canceled by the user.
+              return;
+            }
+            final String filePath = file.path;
+
+            await showDialog<void>(
+              context: context,
+              builder: (BuildContext context) =>
+                  ScannerScreen(imagePath: filePath),
+            );
+          },
+          child: const Text('Pick from gallery'),
+        ),
+      ],
     );
   }
 }
